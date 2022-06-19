3686d3a952573e9201a053dc661f92e8a7bc9702
陈豪恩 <chenhaoen2022@163.com>
2022-06-19 09:57:12 +0800

    1、配置环境

--- TinyGIS/TinyGIS.cpp
+++ TinyGIS/TinyGIS.cpp
@@ -1,7 +1,14 @@
 #include "TinyGIS.h"
 
+#include <qgsmapcanvas.h>
+
 TinyGIS::TinyGIS(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    QgsMapCanvas* map_canvas = new QgsMapCanvas(this);
+
+    setCentralWidget(map_canvas);
+
 }
