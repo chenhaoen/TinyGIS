688bc15357f1395399b9bcd0b8a11f4a2e4209a0
chenhaoen <chenhaoen2022@163.com>
2022-08-31 19:19:37 +0800

    1、重新配置环境

--- TinyGIS/TinyGIS_zh_Hans.ts
+++ TinyGIS/TinyGIS_zh_Hans.ts
@@ -4,18 +4,22 @@
 <context>
     <name>AboutTinyGISDlgClass</name>
     <message>
+        <location filename="AboutTinyGISDlg.ui" line="41"/>
         <source>Author</source>
         <translation>作者</translation>
     </message>
     <message>
+        <location filename="AboutTinyGISDlg.ui" line="69"/>
         <source>Qt version</source>
         <translation>Qt版本</translation>
     </message>
     <message>
+        <location filename="AboutTinyGISDlg.ui" line="20"/>
         <source>chenhaoen</source>
         <translation>陈豪恩</translation>
     </message>
     <message>
+        <location filename="AboutTinyGISDlg.ui" line="55"/>
         <source>QGIS version</source>
         <translation>QGIS版本</translation>
     </message>
@@ -24,10 +28,12 @@
         <translation type="vanished">邮箱</translation>
     </message>
     <message>
+        <location filename="AboutTinyGISDlg.ui" line="14"/>
         <source>About TinyGIS</source>
         <translation>关于TinyGIS</translation>
     </message>
     <message>
+        <location filename="AboutTinyGISDlg.ui" line="48"/>
         <source>email</source>
         <translation>邮箱</translation>
     </message>
@@ -35,10 +41,12 @@
 <context>
     <name>OptionsDlg</name>
     <message>
+        <location filename="OptionsDlg.cpp" line="40"/>
         <source>English</source>
         <translation>英语</translation>
     </message>
     <message>
+        <location filename="OptionsDlg.cpp" line="41"/>
         <source>Chinese</source>
         <translation>中文</translation>
     </message>
@@ -46,14 +54,17 @@
 <context>
     <name>OptionsDlgClass</name>
     <message>
+        <location filename="OptionsDlg.ui" line="30"/>
         <source>Genarel</source>
         <translation>常规</translation>
     </message>
     <message>
+        <location filename="OptionsDlg.ui" line="58"/>
         <source>language</source>
         <translation>语言</translation>
     </message>
     <message>
+        <location filename="OptionsDlg.ui" line="14"/>
         <source>Options</source>
         <translation>选项</translation>
     </message>
@@ -68,30 +79,37 @@
 <context>
     <name>QgsMeasureBase</name>
     <message>
+        <location filename="qgis/ui_qgsmeasurebase.h" line="161"/>
         <source>Measure</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/ui_qgsmeasurebase.h" line="163"/>
         <source>Segments</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/ui_qgsmeasurebase.h" line="164"/>
         <source>Total</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/ui_qgsmeasurebase.h" line="165"/>
         <source>Total Horizontal Distance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/ui_qgsmeasurebase.h" line="166"/>
         <source>Ellipsoidal</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/ui_qgsmeasurebase.h" line="167"/>
         <source>Cartesian</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/ui_qgsmeasurebase.h" line="168"/>
         <source>Info</source>
         <translation type="unfinished"></translation>
     </message>
@@ -99,90 +117,119 @@
 <context>
     <name>QgsMeasureDialog</name>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="49"/>
         <source>&amp;New</source>
         <translation type="unfinished">新建（&amp;N）</translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="54"/>
         <source>&amp;Configuration</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="60"/>
         <source>Copy &amp;All</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="373"/>
         <source>The calculations are based on:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="385"/>
         <source>Cartesian calculation selected, so area is calculated using Cartesian calculations.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="390"/>
         <source>No map projection set, so area is calculated using Cartesian calculations.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="391"/>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="469"/>
         <source>Units are unknown.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="400"/>
         <source>Both project CRS (%1) and measured area are in degrees, so area is calculated using Cartesian calculations in square degrees.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="411"/>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="489"/>
         <source>Project ellipsoidal calculation is selected.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="412"/>
         <source>The coordinates are transformed to the chosen ellipsoid (%1), and the area is calculated in %2.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="418"/>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="496"/>
         <source>Project ellipsoidal calculation is not selected.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="419"/>
         <source>Area is calculated in %1, based on project CRS (%2).</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="422"/>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="500"/>
         <source>Measure</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="446"/>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="524"/>
         <source>The value is converted from %1 to %2.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="463"/>
         <source>Cartesian calculation selected, so distance is calculated using Cartesian calculations.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="468"/>
         <source>No map projection set, so distance is calculated using Cartesian calculations.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="478"/>
         <source>Both project CRS (%1) and measured length are in degrees, so distance is calculated using Cartesian calculations in degrees.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="490"/>
         <source>The coordinates are transformed to the chosen ellipsoid (%1), and the distance is calculated in %2.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="497"/>
         <source>Distance is calculated in %1, based on project CRS (%2).</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="551"/>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="557"/>
         <source>Segments [%1]</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="559"/>
         <source>Segments</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="629"/>
+        <location filename="qgis/qgsmeasuredialog.cpp" line="642"/>
         <source>map units</source>
         <translation type="unfinished"></translation>
     </message>
@@ -190,14 +237,17 @@
 <context>
     <name>QgsMeasureTool</name>
     <message>
+        <location filename="qgis/qgsmeasuretool.cpp" line="85"/>
         <source>Incorrect Measure Results</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuretool.cpp" line="86"/>
         <source>&lt;p&gt;This map is defined with a geographic coordinate system (latitude/longitude) but the map extents suggests that it is actually a projected coordinate system (e.g., Mercator). If so, the results from line or area measurements will be incorrect.&lt;/p&gt;&lt;p&gt;To fix this, explicitly set an appropriate map coordinate system using the &lt;tt&gt;Settings:Project Properties&lt;/tt&gt; menu.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="qgis/qgsmeasuretool.cpp" line="155"/>
         <source>Transform error caught at the MeasureTool: %1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -205,26 +255,32 @@
 <context>
     <name>TinyGIS</name>
     <message>
+        <location filename="TinyGIS.cpp" line="383"/>
         <source>[*]%1 - TinyGIS</source>
         <translation>[*]%1 - TinyGIS</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="173"/>
         <source>Open Project File</source>
         <translation>打开工程文件</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="390"/>
         <source>Save Project File</source>
         <translation>保存工程文件</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="422"/>
         <source>Save current project?</source>
         <translation>是否保存当前工程？</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="456"/>
         <source>Layer View</source>
         <translation>图层视图</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="457"/>
         <source>Log</source>
         <translation>日志</translation>
     </message>
@@ -233,42 +289,52 @@
         <translation type="vanished">打开工程成功</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="311"/>
         <source>Add vector layer</source>
         <translation>添加矢量图层</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="290"/>
         <source>Add raster layer</source>
         <translation>添加栅格图层</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="476"/>
         <source>Open project successfully.</source>
         <translation>打开工程成功。</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="189"/>
         <source>Save project successfully.</source>
         <translation>保存工程成功。</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="197"/>
         <source>Close project successfully.</source>
         <translation>关闭工程成功。</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="355"/>
         <source>Zoom to Layer</source>
         <translation>缩放至图层</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="358"/>
         <source>Remove layer</source>
         <translation>移除图层</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="480"/>
         <source>Failed to open project.</source>
         <translation>打开工程失败。</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="466"/>
         <source>The project file is not a valid file.</source>
         <translation>工程文件不是有效的文件。</translation>
     </message>
     <message>
+        <location filename="TinyGIS.cpp" line="380"/>
         <source>Untitled Project</source>
         <translation type="unfinished">未命名工程</translation>
     </message>
@@ -276,6 +342,7 @@
 <context>
     <name>TinyGISClass</name>
     <message>
+        <location filename="TinyGIS.ui" line="14"/>
         <source>TinyGIS</source>
         <translation>TinyGIS</translation>
     </message>
@@ -288,14 +355,17 @@
         <translation type="vanished">添加栅格图层</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="141"/>
         <source>Add &amp;Vector Layer</source>
         <translation>添加矢量图层（&amp;V)</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="32"/>
         <source>&amp;Layer</source>
         <translation>图层（&amp;L）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="129"/>
         <source>Add &amp;Raster Layer</source>
         <translation>添加栅格图层（&amp;R）</translation>
     </message>
@@ -304,42 +374,52 @@
         <translation type="vanished">图层视图</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="153"/>
         <source>About &amp;TinyGIS</source>
         <translation>关于TinyGIS（&amp;T)</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="39"/>
         <source>&amp;Help</source>
         <translation>帮助（&amp;H）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="45"/>
         <source>&amp;Settings</source>
         <translation>设置（&amp;S）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="165"/>
         <source>&amp;Options</source>
         <translation>选项（&amp;O）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="51"/>
         <source>&amp;Project</source>
         <translation>工程（&amp;P）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="177"/>
         <source>&amp;Open</source>
         <translation>打开（&amp;O）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="189"/>
         <source>&amp;Save</source>
         <translation>保存（&amp;S）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="201"/>
         <source>&amp;New</source>
         <translation>新建（&amp;N）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="209"/>
         <source>&amp;Exit TinyGIS</source>
         <translation>退出TinyGIS（&amp;E）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="221"/>
         <source>&amp;Close</source>
         <translation>关闭（&amp;C）</translation>
     </message>
@@ -348,42 +428,52 @@
         <translation type="vanished">[*]%1 - TinyGIS</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="62"/>
         <source>&amp;View</source>
         <translation>视图（&amp;V）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="233"/>
         <source>&amp;Pan Map</source>
         <translation>平移地图（&amp;P）</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="241"/>
         <source>Pan Map To Selection</source>
         <translation>平移地图到所选内容</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="253"/>
         <source>Zoom In</source>
         <translation>放大</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="265"/>
         <source>Zoom Out</source>
         <translation>缩小</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="277"/>
         <source>Zoom Last</source>
         <translation>上一视图</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="289"/>
         <source>Zoom Next</source>
         <translation>下一视图</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="301"/>
         <source>Refresh</source>
         <translation>刷新</translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="309"/>
         <source>Measure Line</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="TinyGIS.ui" line="314"/>
         <source>Measure Area</source>
         <translation type="unfinished"></translation>
     </message>
